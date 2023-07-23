5139d218f0dc6351c60b1b9a952b239d0c1c4133
sinaa328 <sinaa328@gmail.com>
2023-07-24 00:42:17 +0330

    having issue with csv -.-

--- 3-1.py
+++ 3-1.py
@@ -1,17 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-    
-while True:
+import datetime
+import csv
+
+with open('new.csv', mode='w') as csv_file:
+    fieldnames = ['time', 'price']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
     while True:
-        try:
-            req = requests.get("https://www.tgju.org/")
+        while True:
+            try:
+                    req = requests.get("https://talakar.com/#")
 
-            soup = BeautifulSoup(req.content, "html.parser")
+                    soup = BeautifulSoup(req.content, "html.parser")
 
-            news_link = soup.find_all(class_="info-price")
+                    news_link = soup.find_all(style="cursor: pointer;")
+                    b = str(news_link[0])
+                    c = b.split('<')
+                    d = c[4].split('>')
+                    nowtime = datetime.datetime.now()
+                    print(nowtime)
+                    print(d[1])
+                    writer.writeheader()
+                    writer.writerow({'time' :nowtime ,'price' : d[1] })
+            except:
+                continue
+            time.sleep(10)        
 
-            print(news_link[5])
-        except:
-            continue
-        time.sleep(10)
