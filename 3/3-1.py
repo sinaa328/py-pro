19e791cd9a315e731433b7de295b624538231916
sinaa328 <sinaa328@gmail.com>
2023-07-28 15:49:44 +0330

    fixed and DONE!!!!!1

--- 3/3-1.py
+++ 3/3-1.py
@@ -3,30 +3,33 @@ from bs4 import BeautifulSoup
 import time
 import datetime
 import csv
+count = 0
+with open('new.csv','w',encoding='utf-8',newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['date and time','price'])
+    
+    dollor = None
+    
+    for count in range(50):
+        try:
+            req = requests.get("https://talakar.com/#")
 
-with open('new.csv', mode='w') as csv_file:
-    csv_writer = csv.writer(csv_file,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
-    req = requests.get("https://talakar.com/#")
+            soup = BeautifulSoup(req.content, "html.parser")
 
-    soup = BeautifulSoup(req.content, "html.parser")
-
-    news_link = soup.find_all(style="cursor: pointer;")
-    b = str(news_link[0])
-    c = b.split('<')
-    d = c[4].split('>')
-    nowtime = datetime.datetime.now()
-    csv_writer.writerow([nowtime,d[1]])
-    print(nowtime)
-    print(d[1])
-
-
-    # while True:
-    #     while True:
-    #         try:
-
-
-
-            # except:
-            #     continue
-            # time.sleep(10)        
+            news_link = soup.find_all(style="cursor: pointer;")
+            b = str(news_link[0])
+            c = b.split('<')
+            d = c[4].split('>')
+            if dollor != d[1]:
+                nowtime = datetime.datetime.now()
+                nowtime = str(nowtime)
+                csv_writer.writerow([nowtime[0:19],d[1]])
+                print(nowtime[0:19])
+                print(d[1])
+                dollor = d[1]
+                count += 1
+            time.sleep(10)  
+            
+        except:
+            continue      
 
