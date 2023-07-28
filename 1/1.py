ac7c6a9acbad2440f175724201d62d4452b7f90c
sinaa328 <sinaa328@gmail.com>
2023-07-28 16:20:37 +0330

    prize grade set and DONE!!!!!!!

--- 1/1.py
+++ 1/1.py
@@ -3,7 +3,6 @@ import os
 import shutil
 
 a = os.listdir('./1/Project-s1_Files')
-print(a)
 
 with open(r'./1/Project-s1_Files/persons.csv', encoding="utf-8")as file:
     
@@ -19,5 +18,9 @@ with open(r'./1/Project-s1_Files/persons.csv', encoding="utf-8")as file:
             for i in a:
                 if i[0:10] == lines[0]:
                     shutil.move(f'./1/Project-s1_Files/{i}',path)
+                    old_name = path + '/' + i
+                    new_name = path + '/' + i[11:]
+                    os.rename(old_name,new_name)
                     
+print('Done!')
         
