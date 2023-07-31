c058da9040fa5fe9122b51fc1e9c4e1e9577c362
sinaa328 <sinaa328@gmail.com>
2023-07-31 21:44:58 +0330

    i wish its last edit :)))))))

--- 2/2.py
+++ 2/2.py
@@ -1,12 +1,13 @@
 import csv
 import math
+import heapq
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 job_titles_per_income = {}
-income_per_experince = {'SE' : [0,0], 'EN' : [0,0], 'MI' : [0,0], 'EX' : [0,0]}
+income_per_experience = {'SE' : [0,0], 'EN' : [0,0], 'MI' : [0,0], 'EX' : [0,0]}
 income_per_country = {}
 
 with open(r'./2/salaries_cyber.csv', encoding='utf-8')as file:
@@ -25,8 +26,8 @@ with open(r'./2/salaries_cyber.csv', encoding='utf-8')as file:
                 else:
                     job_titles_per_income[lines[3]] = [int(lines[6]),1]
 
-                income_per_experince[lines[1]][0] += int(lines[6])
-                income_per_experince[lines[1]][1] += 1
+                income_per_experience[lines[1]][0] += int(lines[6])
+                income_per_experience[lines[1]][1] += 1
 
                 if lines[7] in income_per_country:
                     income_per_country[lines[7]][0] += int(lines[6])
@@ -34,6 +35,7 @@ with open(r'./2/salaries_cyber.csv', encoding='utf-8')as file:
                 else:
                     income_per_country[lines[7]] = [int(lines[6]),1]
 
+# first chart: which jobs have how much salary
 
 first_chart_x = []
 first_chart_y = []
@@ -42,28 +44,38 @@ for i in job_titles_per_income:
     first_chart_x.append(i)
     first_chart_y.append(math.floor(job_titles_per_income[i][0]/job_titles_per_income[i][1]))
 
+plt.plot(first_chart_x, first_chart_y)
+
+plt.show()
+
+# second chart: how experience effect on salary
 
 second_chart_x = []
 second_chart_y = []
 
-for i in income_per_experince:
+for i in income_per_experience:
     second_chart_x.append(i)
-    second_chart_y.append(math.floor(income_per_experince[i][0]/income_per_experince[i][1]))
+    second_chart_y.append(math.floor(income_per_experience[i][0]/income_per_experience[i][1]))
 
-
-third_chart_x = []
-third_chart_y = []
-
-# for i in income_per_country:
-
-# print(job_titles_per_income)
-# print(income_per_experince)
-# print(income_per_country)
-
-# fig, ax = plt.subplots()
-
-plt.plot(first_chart_x, first_chart_y)
 plt.plot(second_chart_x,second_chart_y)
 
 plt.show()
 
+# third chart: which country pay mor salaries
+
+third_chart_x = []
+third_chart_y = []
+
+for i in income_per_country:
+    third_chart_x.append(i)
+    third_chart_y.append(math.floor(income_per_country[i][0]/income_per_country[i][1]))
+
+top_income_per_x = []
+top_income_per_y = []
+
+for i in heapq.nlargest(5,third_chart_y):  # this loop is for find top 5 countries
+    top_income_per_x.append(third_chart_x[third_chart_y.index(i)])
+    top_income_per_y.append(i)
+
+plt.plot(top_income_per_x,top_income_per_y)
+plt.show
