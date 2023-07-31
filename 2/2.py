146adf7aa4739e7e61bb56af97b6b10c1098cc20
sinaa328 <sinaa328@gmail.com>
2023-07-31 16:01:03 +0330

    part II in progress....

--- 2/2.py
+++ 2/2.py
@@ -1,7 +1,10 @@
 import csv
 
 
-job_titles = {}
+job_titles_per_income = {}
+income_per_experince = {'SE' : [0,0], 'EN' : [0,0], 'MI' : [0,0], 'EX' : [0,0]}
+income_per_country = {}
+
 with open(r'./2/salaries_cyber.csv', encoding='utf-8')as file:
     csvFile = csv.reader(file)
     count = 0
@@ -12,12 +15,25 @@ with open(r'./2/salaries_cyber.csv', encoding='utf-8')as file:
             if lines[2] != 'FT':
                 continue
             else:
-                if lines[3] in job_titles:
-                    job_titles[lines[3]][0] += int(lines[6])
-                    job_titles[lines[3]][1] += 1
+                if lines[3] in job_titles_per_income:
+                    job_titles_per_income[lines[3]][0] += int(lines[6])
+                    job_titles_per_income[lines[3]][1] += 1
                 else:
-                    job_titles[lines[3]] = [int(lines[6]),1]
+                    job_titles_per_income[lines[3]] = [int(lines[6]),1]
 
-# print(job_titles)
-for i in job_titles:
-    print(job_titles[i][0]/job_titles[i][1])
+                income_per_experince[lines[1]][0] += int(lines[6])
+                income_per_experince[lines[1]][1] += 1
+
+                if lines[7] in income_per_country:
+                    income_per_country[lines[7]][0] += int(lines[6])
+                    income_per_country[lines[7]][1] += 1
+                else:
+                    income_per_country[lines[7]] = [int(lines[6]),1]
+
+# for i in job_titles_per_income:
+#     print(job_titles_per_income[i][0]/job_titles_per_income[i][1])
+
+
+# print(job_titles_per_income)
+# print(income_per_experince)
+# print(income_per_country)
